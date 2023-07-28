6aee10a75a998265dfb2f894a807105720f2ee5e
Thisara MAdusanka <137640738+thisara02@users.noreply.github.com>
2023-07-28 14:02:10 +0530

    Add files via upload

---
+++ Lab  sheet 01  -  Qusetion 01/Lab 01  -  Qusetion 01/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Lab_01_____Qusetion_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string Batch;
+            Console.Write("Enter your name: ");
+            name = Console.ReadLine();
+            Console.Write("Enter your Batch:");
+            Batch = Console.ReadLine();
+        }
+    }
+}
